263226b54346709b2dfb79cff06e825226724d41
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-17 09:39:02 +0200

    todo-backend: Make dueDate optional
    * Follow up to https://github.com/LeddaZ/todo-backend/commit/e7bc9cd255dd7e642adec48cb8f611930d009b5f
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- src/api/todo/todo.dto.ts
+++ src/api/todo/todo.dto.ts
@@ -1,9 +1,10 @@
-import { IsDateString, IsString } from 'class-validator'
+import { IsDateString, IsString, ValidateIf } from 'class-validator'
 
 export class CreateTodoDTO {
   @IsString()
   title: string
 
   @IsDateString()
+  @ValidateIf((_object, value) => value !== undefined)
   dueDate: string
 }
